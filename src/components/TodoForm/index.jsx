2d2ae6b0290dc0068896a44ec11b01fe11efc1d9
Uygar Süslü <uygarsuslu1@gmail.com>
2022-08-02 16:09:59 +0300

    todoform component created

---
+++ src/components/TodoForm/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function index() {
+  return (
+    <form>
+      <input class="new-todo" placeholder="add a todo" autoFocus />
+    </form>
+  );
+}
+
+export default index;
