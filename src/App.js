5d8f3252e1d87b90d9b92c2de7b174a42aaec224
Uygar Süslü <uygarsuslu1@gmail.com>
2022-08-02 15:57:55 +0300

    header component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div >
-      test
-    </div>
+    <section className="todoapp">
+      <Header />
+    </section>
   );
 }
 
