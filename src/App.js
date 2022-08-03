5bac5a23410cbc2234e8c8f88a6511717b7ce03c
Uygar Süslü <uygarsuslu1@gmail.com>
2022-08-03 20:53:55 +0300

    content component created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from "./components/Header";
 import TodoForm from "./components/TodoForm";
+import Content from './components/Content';
 
 function App() {
   return (
     <section className="todoapp">
       <Header />
       <TodoForm />
+      <Content />
     </section>
   );
 }
