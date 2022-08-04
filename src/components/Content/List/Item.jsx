74e3e942e0e04c440c91e47d84de853cc119450a
Uygar Süslü <uygarsuslu1@gmail.com>
2022-08-04 19:06:40 +0300

    checkbox actions

--- src/components/Content/List/Item.jsx
+++ src/components/Content/List/Item.jsx
@@ -1,14 +1,31 @@
 import React from "react";
-import { AiOutlineCheckCircle, AiOutlineCloseCircle } from "react-icons/ai";
+import {
+  AiOutlineCheckCircle,
+  AiOutlineCloseCircle,
+  AiOutlineMinusCircle,
+} from "react-icons/ai";
+import { useTodo } from "../../../contexts/TodoContext";
 
 function Item({ todo }) {
+  const { toggleTodo } = useTodo();
+
+  const onClick = (id) => toggleTodo(id);
+
+  const Icon = todo.completed ? AiOutlineCheckCircle : AiOutlineMinusCircle;
+
   return (
     <li key={todo.id} className={todo.completed ? "completed" : ""}>
       <div className="view">
-        <AiOutlineCheckCircle
+        <Icon
           className="icon"
           type="checkbox"
           checked={todo.completed}
+          onClick={() => onClick(todo.id)}
+          style={{
+            color: `${
+              todo.completed ? "rgb(53, 224, 96)" : "rgb(223, 51, 51)"
+            }`,
+          }}
         />
         <label>{todo.text}</label>
         <AiOutlineCloseCircle className="destroy" />
